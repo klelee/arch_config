224be20f3f05307a78557329c982f62a77188ed3
klelee <klelee@outlook.com>
2022-06-20 14:59:32 +0800

    update repo

--- KDE/install_tiling/install_tiling.sh
+++ KDE/install_tiling/install_tiling.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd Grid-Tiling-Kwin
+pushd Grid-Tiling-Kwin
 kpackagetool5 --type KWin/Script -i ~/Grid-Tiling-Kwin
 mkdir ~/.local/share/kservices5
 cp ~/Grid-Tiling-Kwin/metadata.desktop ~/.local/share/kservices5/kwin-script-grid-tiling.desktop
+popd
